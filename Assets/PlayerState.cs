031ec73599825539fd39c1ae47875337d5cfb771
dimasabreu <dimasabreu@live.com>
2023-04-08 22:51:03 -0300

    stateMachine

--- Assets/PlayerState.cs
+++ Assets/PlayerState.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerState : MonoBehaviour
+public class PlayerState
 {
-    // Start is called before the first frame update
-    void Start()
+    protected PlayerStateMachine stateMachine;
+    protected  Player player;
+    private string animBoolName;
+
+    public PlayerState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName)
     {
-        
+        this.player = _player;
+        this.stateMachine = _stateMachine;
+        this.animBoolName = _animBoolName;
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void Enter() 
     {
-        
+        Debug.Log("I enter" + animBoolName);
+    }
+
+    public virtual void Update()
+    {
+        Debug.Log("I'm in" + animBoolName);
+    }
+
+    public virtual void Exit()
+    {
+        Debug.Log("I exit" + animBoolName);
     }
 }
