0af91c06ce0ce651a80a1856675901113141507c
dimasabreu <dimasabreu@live.com>
2023-04-11 22:52:52 -0300

    player attack

--- Assets/Scripts/Player/PlayerPrimaryAttack.cs
+++ Assets/Scripts/Player/PlayerPrimaryAttack.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerPrimaryAttack : PlayerState
 {
+
+    private int comboCounter;
+    private float lastTimeAttacked;
+    private float comboWindow = 2;
     public PlayerPrimaryAttack(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
     }
@@ -11,16 +15,26 @@ public class PlayerPrimaryAttack : PlayerState
     public override void Enter()
     {
         base.Enter();
+
+        if(comboCounter > 2 || Time.time >= lastTimeAttacked + comboWindow)
+            comboCounter = 0;
+
+        player.anim.SetInteger("ComboCounter", comboCounter);
     }
 
     public override void Exit()
     {
         base.Exit();
+
+
+        comboCounter++;
+        lastTimeAttacked = Time.time;
     }
 
     public override void Update()
     {
         base.Update();
+
         if(triggerCalled)
             stateMachine.ChangeState(player.idleState);
     }
