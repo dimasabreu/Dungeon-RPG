fbc7e099a920869d5980f14285be0e5ca68e1096
dimasabreu <dimasabreu@live.com>
2023-05-08 14:53:28 -0300

    starting blackhole

---
+++ Assets/Scripts/Skills/SkillController/Blackhole_Skill_Controller.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackhole_Skill_Controller : MonoBehaviour
+{
+    public float maxSize;
+    public float growSpeed;
+    public bool canGrow;
+
+    public List<Transform> targets;
+    private void Update() 
+    {
+        if(canGrow)
+        {
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(maxSize, maxSize), growSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if(collision.GetComponent<Enemy>() != null)
+        {
+            collision.GetComponent<Enemy>().FreezeTime(true);
+            //targets.Add(collision.transform);
+        }
+    }
+}
