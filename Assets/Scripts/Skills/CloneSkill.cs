ea839292fecc5084f0c299245a84d77147ddec45
dimasabreu <dimasabreu@live.com>
2023-05-26 19:09:23 -0300

    clone

--- Assets/Scripts/Skills/CloneSkill.cs
+++ Assets/Scripts/Skills/CloneSkill.cs
@@ -9,6 +9,10 @@ public class CloneSkill : Skill
     [SerializeField] private float cloneDuration;
     [Space]
     [SerializeField] private bool canAttack;
+
+    [SerializeField] private bool createCloneOnDashStart;
+    [SerializeField] private bool createCloneOnDashOver;
+    [SerializeField] private bool canCreateCloneOnCounterAttack;
     
 
     public void CreateClone(Transform _clonePosition, Vector3 _offset)
@@ -16,5 +20,29 @@ public class CloneSkill : Skill
         GameObject newClone = Instantiate(clonePrefab);
 
         newClone.GetComponent<Clone_Skill_Controller>().SetupClone(_clonePosition, cloneDuration, canAttack, _offset, FindClosestEnemy(newClone.transform));
+    }
+
+    public void CreateCloneOnDashStart()
+    {
+        if(createCloneOnDashStart)
+            CreateClone(player.transform, Vector3.zero);
+    }
+
+    public void CreateCloneOnDashOver()
+    {
+        if(createCloneOnDashOver)
+            CreateClone(player.transform, Vector3.zero);
+    }
+
+    public void CanCreateCloneOnCounterAttack(Transform _enemyTransform)
+    {
+        if(canCreateCloneOnCounterAttack)
+            StartCoroutine(CreateCloneWithDelay(_enemyTransform, new Vector3(1 * player.facingDir, 0)));
+    }
+
+    private IEnumerator CreateCloneWithDelay(Transform _transform, Vector3 _offset)
+    {
+        yield return new WaitForSeconds(.4f);
+            CreateClone(_transform, _offset);
     }
 }
